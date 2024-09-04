0af8e0c7b01f10fbaa901d50f1c7a3c749469d3d
JordenReuter <149687553+JordenReuter@users.noreply.github.com>
2024-09-04 07:53:20 +0200

    Update BookmarkRestControllerIT.java

--- src/test/java/org/tkit/onecx/bookmark/rs/internal/controllers/BookmarkRestControllerIT.java
+++ src/test/java/org/tkit/onecx/bookmark/rs/internal/controllers/BookmarkRestControllerIT.java
@@ -1,5 +1,7 @@
 package org.tkit.onecx.bookmark.rs.internal.controllers;
 
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
 @QuarkusIntegrationTest
 public class BookmarkRestControllerIT extends BookmarkRestControllerTest {
 }
